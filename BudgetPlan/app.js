79c86e5f40b5f17ae805f4d2323a1bf5918d7eee
LukaRaicevic <luka.raicevic888@gmail.com>
2018-11-19 19:04:29 +0100

    ispravljeno

--- BudgetPlan/app.js
+++ BudgetPlan/app.js
@@ -3,8 +3,14 @@ function $id(id) {
     return document.getElementById(id);
 }
 
+function $create(elem) {
+    return document.createElement(elem);
+}
+
 let incSum = 0;
 let expSum = 0;
+let incArr = [];
+let expArr = [];
 const budget = $id("budget");
 const incHdr = $id("inc");
 const expHdr = $id("exp");
@@ -13,8 +19,7 @@ const greenIcon = $id("green");
 const redIcon = $id("red");
 const tblIncome = $id("income");
 const tblExpenses = $id("expenses");
-const perc = document.getElementById("perc");
-let container = $id("container").innerHTML;
+const perc = $id("perc");
 
 function setDate() {
     const date = $id("date");
@@ -39,6 +44,7 @@ function slct() {
 }
 
 function tableRow(table, sign) {
+    const description = $id("describe").value;
     let val = parseFloat($id("value").value);
     if(select.value === "plus") {
         incSum += val;
@@ -46,17 +52,16 @@ function tableRow(table, sign) {
         expSum += val;
     }
 
-    addRow(table, sign, val);
+    addRow(table, sign, val, description);
 }
 
-function addRow(table, sign, val) {
-    const description = $id("describe");
-    let tr = document.createElement("tr");
-    let td0 = document.createElement("td");
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-    let btn = document.createElement("button");
+function addRow(table, sign, val, description) {
+    let tr = $create("tr");
+    let td0 = $create("td");
+    let td1 = $create("td");
+    let td2 = $create("td");
+    let td3 = $create("td");
+    let btn = $create("button");
     btn.innerHTML = "X";
     btn.addEventListener("click", function() {
         dltBtn(this, sign, val);
@@ -70,7 +75,7 @@ function addRow(table, sign, val) {
         tr.childNodes[1].className = "exp-tbl-val";
         tr.childNodes[2].innerText = percent(val);
     }
-    tr.childNodes[0].innerHTML = description.value;
+    tr.childNodes[0].innerHTML = description;
     tr.childNodes[1].innerHTML = sign+val.toFixed(2);
     tr.childNodes[tr.childNodes.length-1].appendChild(btn);
 }
@@ -82,6 +87,7 @@ function setValue(elem, val, sign) {
         elem.innerHTML = "+ "+Number(val).toFixed(2);
     } else {
         if(sign === "-") {
+            if(val < 0) {sign = ""};
             elem.innerHTML = sign+Number(val).toFixed(2);
             perc.innerHTML = percent(val);
         }
@@ -94,19 +100,30 @@ function dltBtn(elem, sign, val) {
     if(sign === "+") {
         incSum -= val;
         setValue(incHdr, incSum, "+");
-        setValue(budget, incSum-expSum, "sum");
+        if(expSum < 0) {
+            setValue(budget, incSum+expSum, "sum");
+        } else {
+            setValue(budget, incSum-expSum, "sum");
+        }
         perc.innerHTML = percent(expSum);
     } else {
         expSum -= val;
         setValue(expHdr, expSum, "-");
-        setValue(budget, incSum-expSum, "sum");
+        if(expSum < 0) {
+            setValue(budget, incSum+expSum, "sum");
+        } else {
+            setValue(budget, incSum-expSum, "sum");
+        }
         perc.innerHTML = percent(expSum);
     }
     percentTable();
+    hdrToLclStrg();
+    tblToLclStrg(tblIncome);
+    tblToLclStrg(tblExpenses);
 }
 
 function percent(val) {
-    let num = (val/incSum*100).toFixed(1);
+    let num = (val/Number(incHdr.innerHTML)*100).toFixed(1);
     if(isNaN(num)) {
         return "0%";
     } else if(num === "Infinity") {
@@ -124,18 +141,96 @@ function percentTable() {
     }
 }
 
+function hdrToLclStrg() {
+    localStorage.setItem("budget", budget.innerHTML);
+    localStorage.setItem("incomeHdr", incHdr.innerHTML);
+    localStorage.setItem("expensesHdr", expHdr.innerHTML);
+    localStorage.setItem("percHdr", perc.innerHTML);
+    localStorage.setItem("incSum", incHdr.innerHTML);
+    localStorage.setItem("expSum", expHdr.innerHTML);
+}   
+
+function tblToLclStrg(table) {
+    if(table.id === "income") {
+        incArr = [];
+    } else {
+        expArr = [];
+    }
+    for(i = 0; i < table.childElementCount; i++) {
+        if(table.id === "income") {
+            incArr.push((table.children[i].children[0]).innerHTML);
+            incArr.push((table.children[i].children[1]).innerHTML);
+        } else {
+            expArr.push((table.children[i].children[0]).innerHTML);
+            expArr.push((table.children[i].children[1]).innerHTML);
+        }
+    }
+    localStorage.setItem("incTbl", JSON.stringify(incArr));
+    localStorage.setItem("expTbl", JSON.stringify(expArr));
+}
+
+function hdrFromLclStrg() {
+    budget.innerHTML = localStorage.getItem("budget");
+    incHdr.innerHTML = localStorage.getItem("incomeHdr");
+    expHdr.innerHTML = localStorage.getItem("expensesHdr");
+    perc.innerHTML = localStorage.getItem("percHdr");
+}
+
+function tblFromLclStrg(table) {
+    if(localStorage !== null && localStorage !== "" && typeof (Storage) !== undefined) {
+        if(table.id === "income") {
+            if(JSON.parse(localStorage.getItem("incTbl")) !== null) {
+                for(let i = 0; i < JSON.parse(localStorage.getItem("incTbl")).length; i+=2) {
+                    addRow(tblIncome, "+", Number(JSON.parse(localStorage.getItem("incTbl"))[i+1]), JSON.parse(localStorage.getItem("incTbl"))[i]);
+                    percentTable();
+                }
+            }
+        } else {
+            if(JSON.parse(localStorage.getItem("expTbl")) !== null) {
+                for(let i = 0; i < JSON.parse(localStorage.getItem("expTbl")).length; i+=2) {
+                    addRow(tblExpenses, "", Number(JSON.parse(localStorage.getItem("expTbl"))[i+1]), JSON.parse(localStorage.getItem("expTbl"))[i]);
+                    percentTable();
+                }
+            }
+        }
+    }
+}
+
+incArr = JSON.parse(localStorage.getItem("incTbl"));
+expArr = JSON.parse(localStorage.getItem("expTbl"));
+incSum = Number(localStorage.getItem("incSum"));
+expSum = Number(localStorage.getItem("expSum"));
+
 setDate();
 slct();
 greenIcon.addEventListener("click", function() {
     tableRow(tblIncome, "+");
     setValue(incHdr, incSum, "+");
-    setValue(budget, incSum-expSum, "sum");
+    if(expSum < 0) {
+        setValue(budget, incSum+expSum, "sum");
+    } else {
+        setValue(budget, incSum-expSum, "sum");
+    }
     perc.innerHTML = percent(expSum);
     percentTable();
+    hdrToLclStrg();
+    tblToLclStrg(tblIncome);
 });
 redIcon.addEventListener("click", function() {
     tableRow(tblExpenses, "-");
     setValue(expHdr, expSum, "-");
-    setValue(budget, incSum-expSum, "sum");
+    if(expSum < 0) {
+        setValue(budget, incSum+expSum, "sum");
+    } else {
+        setValue(budget, incSum-expSum, "sum");
+    }
+    percentTable();
+    hdrToLclStrg();
+    tblToLclStrg(tblExpenses);
+});
+window.addEventListener("load", function() {
+    hdrFromLclStrg();
+    tblFromLclStrg(tblIncome);
+    tblFromLclStrg(tblExpenses);
 });
 })();
